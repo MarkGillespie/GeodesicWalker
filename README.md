d4eae46deb0915bc1b734559520923c00a7afb53
Mark Gillespie <mark.gillespie81@gmail.com>
2021-01-11 16:00:52 -0500

    Add option to save all frames of walk

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GeodesicWalker
-![Spot the cow walking along spot the cow.](images/spot-on-spot.png)
+![Spot the cow walking along spot the cow.](images/octo-walk.gif)
 A simple project to visualize a mesh walking along geodesics on itself. Powered by [geometry-central](http://geometry-central.net/) and [Polyscope](http://polyscope.run/).
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include "polyscope/polyscope.h"
 #include "polyscope/surface_mesh.h"
 
+#include <fstream>
+#include <string>
+
 #include "args/args.hxx"
 #include "imgui.h"
 
@@ -31,6 +34,8 @@ std::vector<std::array<size_t, 2>> pathEdges;
 // Polyscope visualization handle, to quickly add data to the surface
 polyscope::SurfaceMesh *psMesh, *psSmallMesh;
 polyscope::CurveNetwork *psPath;
+
+std::string frameDir;
 
 glm::mat4 getFrame(Vector3 t, Vector3 n, Vector3 p) {
   t = -t.normalize();
@@ -79,6 +84,7 @@ void step() {
 // A user-defined callback, for creating control panels (etc)
 // Use ImGUI commands to build whatever you want here, see
 // https://github.com/ocornut/imgui/blob/master/imgui.h
+static size_t frameID = 0;
 void myCallback() {
   step();
   Vector3 srcPos = pos.interpolate(geometry->inputVertexPositions);
@@ -89,6 +95,24 @@ void myCallback() {
   psSmallMesh->objectTransform = frame;
 
   polyscope::registerCurveNetwork("Path", path, pathEdges);
+  if (!frameDir.empty()) {
+    std::string idString = std::to_string(frameID);
+    size_t padLen = 15;
+    if (idString.length() <= padLen) {
+      std::string paddedID =
+          std::string(padLen - idString.length(), '0').append(idString);
+      polyscope::screenshot(frameDir + "/frame" + paddedID + ".png", false);
+      frameID++;
+    } else {
+      std::cerr << "Error: id string length > padding length (" << padLen << ")"
+                << std::endl;
+      std::cerr << "\t No longer saving screenshots" << std::endl;
+    }
+  }
+
+  if (ImGui::Button("Print Camera JSON")) {
+    std::cerr << polyscope::view::getCameraJson() << std::endl;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -96,7 +120,14 @@ int main(int argc, char **argv) {
   // Configure the argument parser
   args::ArgumentParser parser("geometry-central & Polyscope example project");
   args::Positional<std::string> inputFilename(parser, "mesh", "A mesh file.");
-
+  args::ValueFlag<std::string> outputPath(parser, "outputPath",
+                                          "Render frames of walk to this path",
+                                          {"outputPath"});
+  args::ValueFlag<int> startFace(parser, "startFace",
+                                 "Face to start walking from", {"startFace"});
+  args::ValueFlag<std::string> cameraPosFile(
+      parser, "cameraPos",
+      "File containing json describing polyscope camera pos", {"cameraPos"});
   // Parse args
   try {
     parser.ParseCLI(argc, argv);
@@ -114,6 +145,11 @@ int main(int argc, char **argv) {
     std::cerr << "Please specify a mesh file as argument" << std::endl;
     return EXIT_FAILURE;
   }
+  if (startFace) {
+    startingFace = args::get(startFace);
+  }
+
+  frameDir = (outputPath) ? args::get(outputPath) : "";
 
   // Initialize polyscope
   polyscope::init();
@@ -129,6 +165,7 @@ int main(int argc, char **argv) {
       polyscope::guessNiceNameFromPath(args::get(inputFilename)),
       geometry->inputVertexPositions, mesh->getFaceVertexList(),
       polyscopePermutations(*mesh));
+  psMesh->setTransparency(0.75);
 
   VertexData<Vector3> smallPositions(*mesh);
   double minY = 0;
@@ -153,6 +190,20 @@ int main(int argc, char **argv) {
 
   glm::mat4 frame = getFrame(t, n, srcPos);
   psSmallMesh->objectTransform = frame;
+
+  if (cameraPosFile) {
+    try {
+      // https://stackoverflow.com/questions/2912520/read-file-contents-into-a-string-in-c
+      std::ifstream ifs(args::get(cameraPosFile));
+      std::string cameraJson((std::istreambuf_iterator<char>(ifs)),
+                             (std::istreambuf_iterator<char>()));
+      polyscope::view::setCameraFromJson(cameraJson, false);
+    } catch (const std::exception &e) {
+      std::cerr << "Error: could not load camera data from " << cameraPosFile
+                << std::endl;
+      std::cerr << e.what() << std::endl;
+    }
+  }
 
   // Give control to the polyscope gui
   polyscope::show();
