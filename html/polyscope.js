8902001f54e29711d96d9e871ff7be2f2ddd70f6
Mark Gillespie <mark.gillespie81@gmail.com>
2021-01-31 12:22:14 -0500

    Use shader to do wireframe

--- html/polyscope.js
+++ html/polyscope.js
@@ -15,20 +15,20 @@ dat.GUI.prototype.removeFolder = function (name) {
 // normals in the shader
 function createMatCapMaterial(tex_r, tex_g, tex_b, tex_k) {
   let vertexShader = `
-        varying vec3 Point;
-        uniform bool shadeSmooth;
+        attribute vec3 barycoord;
+
+        varying vec2 Point;
+        varying vec3 Barycoord;
 
         void main()
         {
             vec3 vNormal = ( mat3( modelViewMatrix ) * normal );
             vNormal = normalize(vNormal);
 
-            if (shadeSmooth) {
-                Point.x = vNormal.x * 0.5 + 0.5;
-                Point.y = vNormal.y * 0.5 + 0.5;
-            } else {
-                Point = (modelViewMatrix * vec4( position, 1.0 )).xyz;
-            }
+            Point.x = vNormal.x * 0.5 + 0.5;
+            Point.y = vNormal.y * 0.5 + 0.5;
+
+            Barycoord = barycoord;
 
             gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
@@ -41,33 +41,46 @@ function createMatCapMaterial(tex_r, tex_g, tex_b, tex_k) {
         uniform sampler2D Matcap_b; // Matcap texture
         uniform sampler2D Matcap_k; // Matcap texture
         uniform vec3 color;
-        uniform bool shadeSmooth;
+        uniform vec3 edgeColor;
+        uniform float edgeWidth;
 
-        varying vec3 Point;
+        varying vec2 Point;
+        varying vec3 Barycoord;
+
+
+        float getEdgeFactor(vec3 UVW, vec3 edgeReal, float width) {
+
+            // The Nick Sharp Edge Function (tm). There are many like it, but this one is his.
+            float slopeWidth = 1.;
+
+            vec3 fw = fwidth(UVW);
+            vec3 realUVW = max(UVW, 1. - edgeReal.yzx);
+            vec3 baryWidth = slopeWidth * fw;
+
+            vec3 end = width * fw;
+            vec3 dist = smoothstep(end - baryWidth, end, realUVW);
+
+            float e = 1.0 - min(min(dist.x, dist.y), dist.z);
+            return e;
+        }
 
         void main(void){
 
-            vec2 n = Point.xy;
 
-            if (!shadeSmooth) {
-                vec3 fdx = vec3(dFdx(Point.x),dFdx(Point.y),dFdx(Point.z));
-                vec3 fdy = vec3(dFdy(Point.x),dFdy(Point.y),dFdy(Point.z));
+            float alpha = getEdgeFactor(Barycoord, vec3(1.,1.,1.), edgeWidth);
 
-                vec3 normal = normalize(cross(fdx,fdy));
-
-                n.x = normal.x * 0.5 + 0.5;
-                n.y = normal.y * 0.5 + 0.5;
-            }
-
-            vec4 mat_r = texture2D(Matcap_r, n);
-            vec4 mat_g = texture2D(Matcap_g, n);
-            vec4 mat_b = texture2D(Matcap_b, n);
-            vec4 mat_k = texture2D(Matcap_k, n);
+            vec4 mat_r = texture2D(Matcap_r, Point);
+            vec4 mat_g = texture2D(Matcap_g, Point);
+            vec4 mat_b = texture2D(Matcap_b, Point);
+            vec4 mat_k = texture2D(Matcap_k, Point);
 
             vec4 colorCombined = color.r * mat_r + color.g * mat_g + color.b * mat_b + 
                                 (1. - color.r - color.g - color.b) * mat_k;
 
-            gl_FragColor = colorCombined;
+            vec4 edgeColorCombined = edgeColor.r * mat_r + edgeColor.g * mat_g + edgeColor.b * mat_b + 
+                                (1. - edgeColor.r - edgeColor.g - edgeColor.b) * mat_k;
+
+            gl_FragColor = (1.-alpha) * colorCombined + alpha * edgeColorCombined;
         }
     `;
 
@@ -78,7 +91,8 @@ function createMatCapMaterial(tex_r, tex_g, tex_b, tex_k) {
       Matcap_b: { value: tex_b },
       Matcap_k: { value: tex_k },
       color: { value: new THREE.Vector3(1, 0, 1) },
-      shadeSmooth: { value: true },
+      edgeColor: { value: new THREE.Vector3(0, 0, 0) },
+      edgeWidth: { value: 0 },
     },
     vertexShader,
     fragmentShader,
@@ -88,10 +102,9 @@ function createMatCapMaterial(tex_r, tex_g, tex_b, tex_k) {
 }
 
 class MeshStructure {
-  constructor(mesh, geo, wireframe, name, polyscopeEnvironment) {
+  constructor(mesh, geo, name, polyscopeEnvironment) {
     this.mesh = mesh;
     this.geo = geo;
-    this.wireframe = wireframe;
     this.name = name;
     this.ps = polyscopeEnvironment;
   }
@@ -113,7 +126,6 @@ class Polyscope {
     this.camera = undefined;
     this.controls = undefined;
     this.shiftClick = false;
-    this.showWireframe = false;
     this.scene = undefined;
     this.matcapTextures = undefined;
 
@@ -220,11 +232,12 @@ class Polyscope {
   }
 
   toggleWireframe(checked, structure) {
-    this.showWireframe = checked;
-    if (this.showWireframe) {
-      structure.mesh.add(structure.wireframe);
+    if (checked) {
+      console.log(structure.mesh.material.uniforms);
+      structure.mesh.material.uniforms.edgeWidth.value = 1;
+      console.log(structure.mesh.material.uniforms);
     } else {
-      structure.mesh.remove(structure.wireframe);
+      structure.mesh.material.uniforms.edgeWidth.value = 0;
     }
   }
 
@@ -261,19 +274,13 @@ class Polyscope {
       this.structureGuiMeshes.open();
     }
     // create THREE.js mesh (and geometry) objects
-    let [threeMesh, threeGeometry, wireframe] = this.constructPolyscopeMesh(
+    let [threeMesh, threeGeometry] = this.constructPolyscopeMesh(
       vertexCoordinates,
       faces,
       scale
     );
 
-    let meshStructure = new MeshStructure(
-      threeMesh,
-      threeGeometry,
-      wireframe,
-      name,
-      this
-    );
+    let meshStructure = new MeshStructure(threeMesh, threeGeometry, name, this);
     this.structures[name] = meshStructure;
 
     let meshGui = this.structureGuiMeshes.addFolder(name);
@@ -355,45 +362,32 @@ class Polyscope {
     // create geometry object
     let threeGeometry = new THREE.BufferGeometry();
 
+    // TODO: handle non-triangular face
     // fill position and color buffers
-    let V = coords.size();
-    let positions = new Float32Array(V * 3);
-    let normals = new Float32Array(V * 3);
-    for (let i = 0; i < V; i++) {
-      let position = coords.get(i);
-      positions[3 * i + 0] = position[0] * scale;
-      positions[3 * i + 1] = position[1] * scale;
-      positions[3 * i + 2] = position[2] * scale;
-    }
-
-    // fill index buffer
     let F = faces.size();
-    let indices = new Uint32Array(F * 3);
-    let maxIndex = 0;
+    let positions = new Float32Array(F * 3 * 3);
+    let normals = new Float32Array(F * 3 * 3);
+    let barycoords = new Float32Array(F * 3 * 3);
     for (let iF = 0; iF < F; iF++) {
-      // TODO: handle non-triangular face
       let face = faces.get(iF);
       for (let iV = 0; iV < 3; iV++) {
-        indices[3 * iF + iV] = face.get(iV);
-        maxIndex = Math.max(maxIndex, face.get(iV));
+        // console.log(iV, face.get(iV), coords.get(face.get(iV)));
+        for (let iD = 0; iD < 3; ++iD) {
+          positions[3 * 3 * iF + 3 * iV + iD] = coords.get(face.get(iV))[iD];
+          barycoords[3 * 3 * iF + 3 * iV + iD] = iD == iV ? 1 : 0;
+        }
       }
     }
 
-    // set geometry
-    threeGeometry.setIndex(new THREE.BufferAttribute(indices, 1));
     threeGeometry.setAttribute(
       "position",
       new THREE.BufferAttribute(positions, 3)
     );
+    threeGeometry.setAttribute(
+      "barycoord",
+      new THREE.BufferAttribute(barycoords, 3)
+    );
     threeGeometry.computeVertexNormals();
-
-    // create wireframe
-    let wireframe = new THREE.LineSegments();
-    wireframe.geometry = new THREE.WireframeGeometry(threeGeometry);
-    wireframe.material = new THREE.LineBasicMaterial({
-      color: 0x000000,
-      linewidth: 0.75,
-    });
 
     // create matcap material
     let matcapMaterial = createMatCapMaterial(
@@ -405,7 +399,7 @@ class Polyscope {
 
     // create mesh
     let threeMesh = new THREE.Mesh(threeGeometry, matcapMaterial);
-    return [threeMesh, threeGeometry, wireframe];
+    return [threeMesh, threeGeometry];
   }
 
   updateMeshColor(meshStructure, color) {
